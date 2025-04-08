b2458ce4d33968a3e4bf511a32c51db132420c61
Hao Nghiem <qtvhao@gmail.com>
2025-04-08 15:21:03 +0700

    Add

--- src/EditVideoDetails.ts
+++ src/EditVideoDetails.ts
@@ -10,10 +10,70 @@ export class EditVideoDetails {
         this.vid = vid;
     }
 
-    async makeChanges() {
+    async makeChanges(title: string, description: string) {
         await this.connect.connectLocalBrowser();
         await this.connect.waitForPageLogin('https://studio.youtube.com/', 'studio.youtube.com');
         const page: Page = await this.connect.getFirstPage();
         await page.goto(`https://studio.youtube.com/video/${this.vid}/edit`);
+        let commonSelector = 'ytcp-form-input-container[focused] #outer.ytcp-form-input-container';
+        await this.typeOnFocused(page, commonSelector, 'Title (required)', title);
+        await this.typeOnFocused(page, commonSelector, 'Description', description);
+    }
+
+    async typeOnFocused(page: Page, selector: string, matcher: string, text: string) {
+        for (let i = 0; i < 200; i++) {
+            await page.keyboard.press('Tab');
+            let $el = await page.$(selector);
+            if ($el) {
+                console.log('Focused:', $el);
+                let borderColor = await page.evaluate(el => {
+                    return getComputedStyle(el).borderColor;
+                }, $el);
+                let innerText = await page.evaluate(el => {
+                    return (el as HTMLElement).innerText;
+                }, $el);
+                let inputsText = await page.evaluate(el => {
+                    let inputs = Array.from(el.querySelectorAll('input'));
+                    return inputs.map(input => input.placeholder);
+                }, $el);
+                innerText += " " + inputsText.join(' ');
+                if (innerText.includes(matcher)) {
+                    console.log('Typing:', text);
+                    for (let i = 0; i < 5200; i++) {
+                        await page.keyboard.press('ArrowRight');
+                        await page.keyboard.press('Backspace');
+                    }
+                    await page.keyboard.type(text);
+                    break;
+                } else {
+                    console.log('Inner text:', innerText);
+                    console.log('Border color:', borderColor);
+                }
+            }
+        }
+    }
+
+    async clickButtonSave(page: Page, immediately_break = false) {
+        let saveButton;
+        while (typeof saveButton === 'undefined') {
+            await new Promise(resolve => setTimeout(resolve, 10_000));
+            console.log('Waiting for save button');
+            let filledButtons = await page.$$('ytcp-button[type=filled]');
+            for (let button of filledButtons) {
+                let text = await page.evaluate(el => (el instanceof HTMLElement) ? el.innerText : '', button);
+                if (text && text.toUpperCase() === 'SAVE') {
+                    saveButton = button;
+                    break;
+                }
+            }
+            if (immediately_break) {
+                break;
+            }
+        }
+        if (saveButton) {
+            await saveButton.click();
+        } else {
+            console.warn('Save button not found');
+        }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import { EditVideoDetails } from "./EditVideoDetails.js";
 import { SaveVideo } from "./SaveVideo.js";
+import { YouTubeUploader } from "./YouTubeUploader.js";
 
-(async () => {
-    const vid = '-leSh7zUV1E'
-
-    console.log({vid})
-
-    const editor = new EditVideoDetails(vid)
-
-    await editor.makeChanges()
-
-})();
+export {
+    YouTubeUploader,
+    SaveVideo,
+    EditVideoDetails,
+}
